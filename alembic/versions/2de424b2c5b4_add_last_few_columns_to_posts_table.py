819287894036c85341981ef73f6baed23540e5c2
sowhat1234 <emiliu1234e@gmail.com>
2021-12-17 14:45:58 +0200

    added commas to add last few columns

--- alembic/versions/2de424b2c5b4_add_last_few_columns_to_posts_table.py
+++ alembic/versions/2de424b2c5b4_add_last_few_columns_to_posts_table.py
@@ -20,10 +20,10 @@ depends_on = None
 def upgrade():
     op.add_column('posts', sa.Column(
         'published', sa.Boolean(), nullable=False, server_default='TRUE'
-    ),)
+    ))
     op.add_column('posts', sa.Column(
         'created_at', sa.TIMESTAMP(timezone=True), nullable=False, server_default=sa.text('NOW()')
-    ),)
+    ))
     pass
 
 
